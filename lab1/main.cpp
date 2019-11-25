6c99dc64b6b987d8cc01566687ff4e7ee3a48b7f
Miheel <mikaelleuf@gmail.com>
2019-11-25 20:32:18 +0100

    commented the code and redid makefiles

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -8,24 +8,29 @@
 
 int main()
 {
+	//initialize some values
 	int min = std::numeric_limits<int>::max(); //assign min the biggest posible int 
 	int max = std::numeric_limits<int>::min(); //assign max the smalest posible int
 	int temp = 0, sum = 0, counter = 0;
 	double mean = 0;
 
-	while(std::cin >> temp) //as long cin gets ints as input continue the loop
+	while(std::cin >> temp) //as long cin gets valid inputs continue the loop
 	{
-		sum += temp;
+		sum += temp; //increase the sum by the value of temp
 
-		if(max < temp)
+		if(max < temp) //if temp has a greater value then max givemax that value
 			max = temp;
-		if(min > temp)
+		
+		if(min > temp) //if temp has a smaller value then min give min that value
 			min = temp;
-		counter++;
+
+		counter++; //counter for the amount of read values
 	}
 
-	mean = static_cast<double>(sum)/counter;
+	mean = static_cast<double>(sum)/counter; //cast sum and calculate the mean value
 
+	//print all results
 	std::cout << "sum: " << sum << " max: " << max << " min: " << min << " mean: " << mean << std::endl;
 
+	return 0;
 }
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
 #include <fstream>
+
+//Namn: Mikael Leuf
+//Datum: 2019-11-25
+//Kurs: DT028G_DT178G - Introduktion till programering
+//Lab: lab2
 
 int main()
 {
@@ -7,22 +13,28 @@ int main()
 	std::string foreName, surName, personID, road;
 	std::string gender = " [ ]";
 	
+	//open the file for reading
 	inFile.open("names.txt");
 	
-	while(getline(inFile, foreName, ' '))
+	while(getline(inFile, foreName, ' ')) //read to the next space in to foreNme
 	{
-		getline(inFile, surName);
-		getline(inFile, personID);
+		getline(inFile, surName); //read the rest of the line in to surName
+		getline(inFile, personID);//read the other lines to the assigned variables
 		getline(inFile, road);
 		
+		//cast the 9th character to an int and subtract the ascii value of 0 to get the value between 0-9
+		//determine if the number if evenly divisible by 2
+		//if thats the case set gender to 'F' otherwise set 'M'
 		if ((static_cast<int>(personID[8]) - '0') % 2 == 0)
 			gender[2] = 'F';
 		else
 			gender[2] = 'M';
-
+		
+		//print the names
 		std::cout << surName << ", " << foreName << gender << std::endl << road << std::endl;
 	}
-
+	
+	//close the file
 	inFile.close();
 	return 0;
 }
